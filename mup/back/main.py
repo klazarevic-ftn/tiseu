2ed262af2c096cf96ece0865a30598a94780a1ff
lazarevic-konstantin <127831884+klazarevic-ftn@users.noreply.github.com>
2024-04-23 00:16:50 +0200

    docker fin

--- mup/back/main.py
+++ mup/back/main.py
@@ -1,15 +1,33 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+class Order(BaseModel):
+    orderId: str
+    caseNo: str
+    executed: bool = False
+
+    def to_json(self):
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__,
+            sort_keys=True)
+
+
+@app.get('/')
+async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post('/order')
+async def receive_order(order: Order):
+    return
+
+
+@app.patch('/order/{orderId}')
+async def execute_order(order_id):
+    return order_id
